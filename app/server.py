a52f59e93ad19aae5a6cbb2731aa74e2af49edf1
Desmic <shashank0218@gmail.com>
2026-09-12 14:45:28 +0530

    feat: serve Play Canvas assets locally

--- app/server.py
+++ app/server.py
@@ -100,6 +100,8 @@ def make_server(database, port=8000):
                 "/rescue-game.js": ("rescue-game.js", "text/javascript"),
                 "/rescue.js": ("rescue.js", "text/javascript"),
                 "/rescue.css": ("rescue.css", "text/css"),
+                "/play-canvas.js": ("play-canvas.js", "text/javascript"),
+                "/play-canvas.css": ("play-canvas.css", "text/css"),
                 "/rescue-intro.js": ("rescue-intro.js", "text/javascript"),
                 "/rescue-intro.css": ("rescue-intro.css", "text/css"),
                 "/rescue-story3d.js": ("rescue-story3d.js", "text/javascript"),
