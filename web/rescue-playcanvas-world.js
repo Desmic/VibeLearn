9c3f0e35a68cc00048f7f26e738863ab86e60869
Desmic <shashank0218@gmail.com>
2026-09-12 20:34:53 +0530

    game: bind Echo Forge pc-phase1-5

--- web/rescue-playcanvas-world.js
+++ web/rescue-playcanvas-world.js
@@ -5,7 +5,7 @@ import {echoForgeWorldSpec} from './echo-forge-world-spec.js';
 
 export const gameWorldManifest=Object.freeze({
   id:'relay-rescue.echo-forge',
-  version:'pc-phase1-4',
+  version:'pc-phase1-5',
   engine:'playcanvas',
   specVersion:echoForgeWorldSpec.schemaVersion,
   modes:Object.freeze(['story','mission']),
